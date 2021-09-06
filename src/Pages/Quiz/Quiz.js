58414161cfeaa4dbab718ef8397d3ebb6f0e9e43
Mehdih77 <mehdiwork77@gmail.com>
2021-09-06 23:38:58 +0430

    shuffle questions in Quiz.js and handle it

--- src/Pages/Quiz/Quiz.js
+++ src/Pages/Quiz/Quiz.js
@@ -1,6 +1,27 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
 import './Quiz.css';
 
 export default function Quiz({name,questions,setQuestions,score,setScore}) {
+
+    const [options, setOptions] = useState();
+    const [currentQues, setCurrentQues] = useState(0);
+
+    const handleShuffle = (optionsData) => {
+        return optionsData.sort(() => Math.random() - 0.5);
+    }
+
+    useEffect(() => {
+        setOptions(
+            questions &&
+            handleShuffle([
+                questions[currentQues]?.correct_answer,
+                ...questions[currentQues]?.incorrect.answer,
+            ])
+        );
+    }, [questions])
+
+
     return (
         <div>
             quiz
